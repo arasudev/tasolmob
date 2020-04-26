876768498c60600ae669587244658441e31b375f
Poovarasu <poovarasu@mallow-tech.com>
2020-04-26 11:46:01 +0530

    completed migration of all the files from galio

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import GalioApp from './routes';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+        <View style={{ flex: 1 }}>
+          <StatusBar hidden={false} />
+          <GalioApp />
+        </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
